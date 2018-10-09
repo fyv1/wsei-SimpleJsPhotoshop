ad29715cde315e14953a27855c3d6e9922d9b685
Nowak Grzegorz <grzegorz.nowak@microsoft.wsei.edu.pl>
2018-10-09 17:13:46 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let img;
+let imgUrl;
+
+let cv = document.querySelector("#cv");
+
+
+
+img = document.createElement('img');
+img.src = imgUrl;
+img.addEventListener('load', function(e) {
+    
+});
