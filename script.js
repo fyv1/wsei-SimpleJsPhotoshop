6c20f389f2a2043edfa92638643e3ad2677a535a
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2018-10-16 23:12:46 +0200

    Added basic functions; TODO: improving mechanism

--- script.js
+++ script.js
@@ -11,7 +11,62 @@ img.src = imgUrl;
 
 img.addEventListener('load', function(e) {
     context.drawImage(img,0,0);
+}
+);
 
+
+document.querySelector("#contrast").addEventListener("input", function(){ 
+    contrast(document.querySelector("#contrast").value);
+});
+
+document.querySelector("#brightness").addEventListener("input", function(){ 
+    brightness(document.querySelector("#brightness").value);
+});
+
+document.querySelector("#saturation").addEventListener("input", function(){ 
+    saturation(document.querySelector("#saturation").value);
+});
+
+document.querySelector("#inversion").addEventListener("click", function(){
+    inversion();
+});
+
+
+function contrast(c) {
+    let imgData =  context.getImageData(0, 0, cv.width, cv.height);
+    let factor = (259 * (c + 255)) / (255 * (259 - c));
+
+    var i;
+    for (i = 0; i < imgData.data.length; i += 4) {
+        imgData.data[i] = factor*(imgData.data[i]-128) + 128;
+        imgData.data[i+1] = factor*(imgData.data[i+1]-128) + 128;
+        imgData.data[i+2] = factor*(imgData.data[i+2]-128) + 128;
+        imgData.data[i+3] = 255;
+    }
+       
+    console.log(factor);
+    context.putImageData(imgData, 0, 0);
+}   
+
+function brightness(b) {
+    let imgData =  context.getImageData(0, 0, cv.width, cv.height);
+
+    var i;
+    for (i = 0; i < imgData.data.length; i += 4) {
+        imgData.data[i] += b;
+        imgData.data[i+1] += b;
+        imgData.data[i+2] += b;
+        imgData.data[i+3] = 255;
+    }
+    console.log(b);
+    context.putImageData(imgData, 0, 0);
+}
+
+function saturation() {
+
+}
+
+function inversion() {
     let imgData =  context.getImageData(0, 0, cv.width, cv.height);
 
     var i;
@@ -21,27 +76,5 @@ img.addEventListener('load', function(e) {
         imgData.data[i+2] = 255 - imgData.data[i+2];
         imgData.data[i+3] = 255;
     }
-context.putImageData(imgData, 0, 0);
+    context.putImageData(imgData, 0, 0);
 }
-);
-
-
-/*
-let cValue = document.querySelector("#cValue");
-let bValue = document.querySelector("#bValue");
-let sValue = document.querySelector("#sValue");
-
-let cContrast = document.querySelector("#contrast");
-cValue.innerHTML = cContrast.value;
-
-let cBrightness = document.querySelector("#brightness");
-bValue.innerHTML = cBrightness.value;
-
-let cSaturation = document.querySelector("#saturation");
-sValue.innerHTML = cSaturation.value;
-
-cContrast.oninput = function() { cValue.innerHTML = this.value; }
-cBrightness.oninput = function() { bValue.innerHTML = this.value; }
-cSaturation.oninput = function() { sValue.innerHTML = this.value; } 
-
-*/
